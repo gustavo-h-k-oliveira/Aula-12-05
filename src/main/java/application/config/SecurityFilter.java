d0210a527cd63e4a4e9ce637b2624d71c544d418
Gustavo Oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2025-05-13 19:48:45 +0000

    Configurando Swagger-UI.

--- src/main/java/application/config/SecurityFilter.java
+++ src/main/java/application/config/SecurityFilter.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
+// import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -32,6 +32,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         return null;
     }
 
+    @SuppressWarnings("null")
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
--- src/main/java/application/controller/CategoriaController.java
+++ src/main/java/application/controller/CategoriaController.java
@@ -2,14 +2,21 @@ package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.record.CategoriaDTO;
 import application.service.CategoriaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Categorias")
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
@@ -21,8 +28,20 @@ public class CategoriaController {
         return categoriaService.getAll();
     }
 
+    @Operation(summary = "Cria uma nova categoria", description = "Retorna o objeto `categoria` criado")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "403", description = "Acesso Não Autorizado"),
+        @ApiResponse(responseCode = "200", description = "Categoria Criada com Sucesso")
+    })
     @PostMapping
     public CategoriaDTO insert(@RequestBody CategoriaDTO categoria) {
         return categoriaService.insert(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public CategoriaDTO update(
+        @RequestBody CategoriaDTO categoria,
+        @PathVariable long id) {
+        return categoria;
     }
 }
